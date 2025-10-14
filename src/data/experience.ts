8ceeb31ea24b578eb1dea27adf052f7257b84532
AIT OUAHDA YOUNES <95384537+YounesAO@users.noreply.github.com>
2025-10-14 20:15:06 +0100

    Update experience.ts

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -54,5 +54,19 @@ export const experiences: Experience[] = [
       'Implemented real-time reporting and analytics for business insights'
     ],
     technologies: ['PHP', 'MySQL', 'Chart.js', 'JavaScript', 'HTML/CSS']
-  }
+  },{
+  id: 4,
+  title: 'Software Architecture Intern',
+  company: 'DeepLeaf',
+  location: 'Morocco',
+  period: 'Jul 2025 - Sep 2025',
+  type: 'internship',
+  description: [
+    'Contributed to the strategic migration of a monolithic API to a scalable microservices architecture to meet B2B/B2G demands.',
+    'Designed and developed a high-performance Usage Log microservice using FastAPI, exposing both REST and gRPC APIs.',
+    'Engineered a specialized data layer with TimescaleDB for time-series data and PostGIS for complex geospatial analysis.',
+    'Implemented advanced features including multi-dimensional filtering and efficient keyset pagination to optimize performance over large datasets.'
+  ],
+  technologies: ['Microservices', 'FastAPI', 'gRPC', 'TimescaleDB', 'PostGIS', 'Docker', 'SQLAlchemy', 'Alembic']
+}
 ];
